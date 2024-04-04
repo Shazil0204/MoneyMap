2aeb4816a5b2fe6c8174276b7ade173252a6fc88
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-04-04 10:29:20 +0200

    Update defaultnavbar.tsx

--- MoneyMapping/src/Components/defaultnavbar.tsx
+++ MoneyMapping/src/Components/defaultnavbar.tsx
@@ -1,85 +1,50 @@
 import { Link } from "react-router-dom";
-import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { faCoins, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Services from "../Pages/Services";
 
 export default function DefaultNavbar() {
-//   let navlinks = [
-//     { text: "Home", linkto: "/" },
-//     { text: "About", linkto: "/about" },
-//     { text: "Services", linkto: "/services" },
-//     { text: "Contact", linkto: "/Contact" },
-//     { text: "Login", linkto: "/Login" },
-//     { text: "Sign up", linkto: "/sign_up" },
-//   ];
+  let navlinks = [
+    { text: "Home", linkto: "/" },
+    { text: "About", linkto: "/about" },
+    { text: "Services", linkto: "/services" },
+    { text: "Contact", linkto: "/Contact" },
+    { text: "Login", linkto: "/Login" },
+    { text: "Register", linkto: "/Register" },
+  ];
 
-//   return (
-//     <>
-//       <div className="p-5 bg-gray-200 fixed w-full rounded-b-lg drop-shadow-2xl">
-//         <div className="flex justify-between">
-//           <Link to={"/"}>
-//             <div className="m-2 flex gap-2 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mr-[1vw] text-blue-800 cursor-pointer">
-//               <FontAwesomeIcon icon={faCoins} className="mt-1" />
-//               <p className="pacifico-regular font-extrabold">Money Map</p>
-//             </div>
-//           </Link>
-//           <ul className="flex gap-4 items-center">
-//             {navlinks.map((nav) => (
-//               <Link to={nav.linkto} key={nav.text} className="p-2">
-//                 {nav.text}
-//               </Link>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//       <div className="p-16"></div>
-//     </>
-//   );
-// }
-return (
-  <>
-    <nav className="bg-gray-800 py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Project Name */}
-        <Link to="/" className="text-white hover:text-gray-300">
-          Task Manager
-        </Link>
-
-        {/* Navigation Links */}
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="text-white hover:text-gray-300">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="text-white hover:text-gray-300">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/Services" className="text-white hover:text-gray-300">
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="text-white hover:text-gray-300">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link to="/Register" className="text-white hover:text-gray-300">
-              Register
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="text-white hover:text-gray-300">
-              Login
-            </Link>
-          </li>
-        </ul>
+  return (
+    <>
+      <div className="p-5 bg-gray-200 fixed w-full rounded-b-lg drop-shadow-2xl z-10">
+        <div className="flex justify-between">
+          <Link to={"/"}>
+            <div className="m-2 flex gap-2 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-3xl mr-[1vw] text-blue-800 cursor-pointer">
+              <FontAwesomeIcon icon={faCoins} className="mt-1" />
+              <p className="pacifico-regular font-extrabold">Money Map</p>
+            </div>
+          </Link>
+          <div className="hidden lg:block">
+            <ul className="flex gap-4 items-center text-2xl">
+              {navlinks.map((nav) => (
+                <Link to={nav.linkto} key={nav.text} className="p-2">
+                  {nav.text}
+                </Link>
+              ))}
+            </ul>
+          </div>
+          <div className="lg:hidden flex items-center justify-center">
+            {/* <FontAwesomeIcon icon={faBars} /> */}
+            <ul className=" gap-4 flex flex-col items-center text-2xl">
+              {navlinks.map((nav) => (
+                <Link to={nav.linkto} key={nav.text} className="p-2">
+                  {nav.text}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </nav>
-  </>
-);
+      <div className="p-16"></div>
+    </>
+  );
 }
+
